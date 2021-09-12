d4a0f014efb4d48e6010e9177e44a837843d1458
Cristian Funck <cefunck@miuandes.cl>
2021-09-11 23:37:04 -0300

    feat(header): resize header on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $("header").css("height","7rem");
+  } else {
+    $("header").css("height","10rem");
+  }
+}
