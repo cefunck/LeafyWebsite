41576878f6d807b2fbb95a1ffb5a3b621397bb7b
Cristian Funck <cefunck@miuandes.cl>
2021-09-11 21:52:14 -0300

    fix(navbar animation): remove script for scroll navbar animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,1 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() == 0) {
-        //Do whatever you want to do
-        $("nav").css("height","15%");
-    }
-    else {
-      $("nav").css("height","10%");
-    }
-});
+
